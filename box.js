d52af8bfcf441d6e10fbac5712d269abb81ddf29
Irene Liao <86861359+iireneliao@users.noreply.github.com>
2021-09-22 19:34:06 -0700

    Create box.js

---
+++ box.js
@@ -0,0 +1,28 @@
+/* global fill, rect, Matter, push, translate, rotate, pop, rectMode, CENTER, world, imageMode, image
+pig
+*/
+
+class Box {
+  constructor(x, y, w, h) {
+    const options = {
+      restitution: 0.5
+    };
+    this.body = Matter.Bodies.rectangle(x, y, w, h, options);
+    Matter.World.add(world, this.body);
+    this.w = w;
+    this.h = h;
+  }
+
+  show() {
+    const pos = this.body.position;
+    const angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    fill(255);
+    rectMode(CENTER);
+    imageMode(CENTER);
+    image(pig, 0, 0, this.w, this.h);
+    pop();
+  }
+}
