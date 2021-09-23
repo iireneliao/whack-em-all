875fe0cbac89bf5f21860ed68e7243708da51eed
Irene Liao <86861359+iireneliao@users.noreply.github.com>
2021-09-22 19:34:53 -0700

    Create slingshot.js

---
+++ slingshot.js
@@ -0,0 +1,36 @@
+/* global Constraint, World, world, stroke, strokeWeight, line
+*/
+
+class SlingShot {
+  constructor(x, y, body) {
+    const options = {
+      pointA: {
+        x: x,
+        y: y
+      },
+      bodyB: body,
+      stiffness: 0.02,
+      length: 10
+    };
+    this.sling = Constraint.create(options);
+    World.add(world, this.sling);
+  }
+
+  fly() {
+    this.sling.bodyB = null;
+  }
+
+  show() {
+    if (this.sling.bodyB) {
+      stroke(0);
+      strokeWeight(3);
+      const posA = this.sling.pointA;
+      const posB = this.sling.bodyB.position;
+      line(posA.x, posA.y, posB.x, posB.y);
+    }
+  }
+
+  attach(body) {
+    this.sling.bodyB = body;
+  }
+}
